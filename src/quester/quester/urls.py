6f14ac06ab0a9000ea7d792aff7886f0daa1a482
OShalakhin <olexandr@shalakhin.com>
2012-08-18 15:41:32 +0300

    update urls.py

--- src/quester/quester/urls.py
+++ src/quester/quester/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     # url(r'^$', 'quester.views.home', name='home'),
     # url(r'^quester/', include('quester.foo.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'defaults.views.home', name='home'),
 )
